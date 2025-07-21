3b9fa19ce4d12053ea841c68c5894be5f6725837
Pau Segués Vitutia <psv.segues@gmail.com>
2025-07-21 16:08:38 +0200

    weatherTextMap const

---
+++ src/consts/weatherTextMap.ts
@@ -0,0 +1,24 @@
+import type { WeatherTextMap } from "@/types/weatherTextMap";
+
+export const weatherTextMap: WeatherTextMap = {
+    0: "☀️ Clear",
+    1: "🌤️ Mostly clear",
+    2: "⛅ Partly cloudy",
+    3: "☁️ Cloudy",
+    45: "🌫️ Fog",
+    48: "🌫️ Fog with frost",
+    51: "🌦️ Light drizzle",
+    53: "🌦️ Moderate drizzle",
+    55: "🌧️ Heavy drizzle",
+    61: "🌧️ Light rain",
+    63: "🌧️ Moderate rain",
+    65: "🌧️🌧️ Heavy rain",
+    66: "🌧️❄️ Light freezing rain",
+    67: "🌧️❄️❄️ Heavy freezing rain",
+    71: "🌨️ Light snow",
+    73: "🌨️ Moderate snow",
+    75: "❄️❄️ Heavy snow",
+    95: "⛈️ Thunderstorm",
+    96: "⛈️🧊 Thunderstorm with hail",
+    99: "⛈️🧊🧊 Severe thunderstorm with hail",
+};
---
+++ src/types/weatherTextMap.ts
@@ -0,0 +1,3 @@
+export interface WeatherTextMap {
+  [key: number]: string;
+}
